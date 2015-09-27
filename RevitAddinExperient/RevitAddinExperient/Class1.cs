d48f4d6d6a91c707119d8af4a9780f4eff2125ea
ipeerbhai <ipeerbhai@aol.com>
2015-09-26 23:54:01 -0700

    Can not find doors in a model.

--- RevitAddinExperient/RevitAddinExperient/Class1.cs
+++ RevitAddinExperient/RevitAddinExperient/Class1.cs
@@ -18,27 +18,39 @@ public class Lab1PlaceGroup : IExternalCommand
 {
     public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
     {
+        Tracelog("Addin Start");
+
         //Get application and document objects
-        UIApplication uiApp = commandData.Application;
-        Document doc = uiApp.ActiveUIDocument.Document;
+        UIApplication UIApp = commandData.Application;
+        UIDocument UIDoc = UIApp.ActiveUIDocument;
+        Application app = UIApp.Application;
+        Document doc = UIDoc.Document;
 
-        //Define a Reference object to accept the pick result.
-        Reference pickedRef = null;
+        // Get all doors.
+        FilteredElementCollector collectorForDoors = new FilteredElementCollector(doc);
+        collectorForDoors.OfCategory(BuiltInCategory.OST_Doors).OfClass(typeof(FamilyInstance));
 
-        //Pick a group
-        Selection sel = uiApp.ActiveUIDocument.Selection;
-        pickedRef = sel.PickObject(ObjectType.Element, "Please select a group");
-        Element elem = doc.GetElement(pickedRef);
-        Group group = elem as Group;
+        // Iterate the collector and show the information
+        if ( collectorForDoors.Count<Element>() > 0 )
+        {
+            // I have a valid collection to iterate
+            foreach(Element ele in collectorForDoors)
+            {
+                TaskDialog.Show(ele.Category.Name, ele.Name);
+            }
+        }
+        else
+        {
+            // No collection found.
+            Tracelog("No collection found");
+        }
 
-        //Pick a point
-        XYZ point = sel.PickPoint("Please pick a point to place group");
-
-        //Place the group
-        Transaction trans = new Transaction(doc);
-        trans.Start("Lab");
-        doc.Create.PlaceGroup(point, group.GroupType);
-        trans.Commit();
         return (Result.Succeeded);
+    }
+
+    public void Tracelog(string msg)
+    {
+        TaskDialog.Show("Debug Message", msg);
+
     }
 }
